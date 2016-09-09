79398bb123f6553c9545b6d4c30f891221abaa16
Helias <stefanoborzi32@gmail.com>
2016-09-10 01:29:06 +0200

    Importing project

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# BG-Queue-Abuser-Viewer
-Tool to check battleground queue abuse
+# BG Queue Abuser Viewer
+
+This tool works with the [TC-JSON-API](https://github.com/ShinDarth/TC-JSON-API.git) and it is compatible with [AzerothCore](https://github.com/azerothcore/azerothcore-wotlk) and [TrinityCore](https://github.com/TrinityCore/TrinityCore).
+
+# ![BG Queue Abuser Viewer](https://raw.githubusercontent.com/Helias/BG-Queue-Abuser-Viewer/master/img/screen.png)
+
+## Contribute
+
+You can help us [opening a new issue](https://github.com/Helias/BG-Queue-Abuser-Viewer/issues/new) to report a bug or a suggestion
+
+or you can donate to support us
+
+[![Donate](https://www.paypal.com/en_GB/i/btn/btn_donateCC_LG.gif "Donate")](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=52AZFFD86N39Q)
---
+++ js/app/app.js
@@ -0,0 +1,51 @@
+(function () {
+  'use strict';
+
+  var app = angular.module('abuserViewer', ['ui.router', 'ui.bootstrap', 'chieffancypants.loadingBar', 'ngAnimate', 'tableSort']);
+
+  app.controller('playerController', function($rootScope, $scope, $http, $stateParams) {
+
+    $rootScope.status = false;
+
+     $rootScope.from = $stateParams.from == null ? 0 : $stateParams.from;
+     $rootScope.name = $stateParams.name == null ? '' : $stateParams.name;
+
+     $rootScope.search = $rootScope.name;
+
+    /* Retrieve all achievement_progress data */
+    $http.get( app.api + "battleground/deserters/recent/50?from=" + $rootScope.from + "&name=" + $rootScope.name)
+      .success(function (data, status, header, config) {
+      $scope.players = data;
+
+      for (var i = 0; i < $scope.players.length; i++) {
+        switch($scope.players[i].type) {
+          case 0:
+          $scope.players[i].status = "blue";
+          $scope.players[i].popover = "il giocatore abbandona il BG (deserter)";
+          break;
+          case 1:
+            $scope.players[i].status = "grey";
+            $scope.players[i].popover = "il giocatore è stato buttato fuori dal BG dopo essere stato offline per un certo numero di minuti (deserter oppure player crashato)";
+          break;
+          case 2:
+            $scope.players[i].status = "red";
+            $scope.players[i].popover = "il giocatore è stato invitato a joinare il BG ma si rifiuta esplicitamente di farlo, cliccando nel pulsante (queue abuser)";
+          break;
+          case 3:
+            $scope.players[i].status = "orange";
+            $scope.players[i].popover = "il giocatore è stato invitato a joinare il BG ma non preme nessun pulsante e scade il tempo (AFK oppure queue abuser)";
+          break;
+          case 4:
+            $scope.players[i].status = "yellow";
+            $scope.players[i].popover = "il giocatore è stato invitato a joinare BG ed in quel momento slogga (queue abuser, oppure per una rara coincidenza il player deve sloggare proprio in quel momento)";
+          break;
+        }
+      }
+    })
+      .error(function (data, status, header, config) {
+      console.log("[ERROR] $http.get request failed in playerController!");
+    });
+
+  });
+
+}());
