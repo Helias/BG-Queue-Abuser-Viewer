3dac225e802d4fdeee691b763aa9ee021136e2a8
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2022-06-12 21:26:15 +0200

    Added table. Started defining methods for interacting with it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'BG Queue Abuser Viewer'
+export class AppComponent implements AfterViewInit {
+  title = 'BG Queue Abuser Viewer';
+  displayedColumns: (keyof Row)[] = ['guid', 'account', 'name', 'level', 'type', 'datetime'];
+  entriesPerPage: number = 25;
+  currentData: Row[] = [];
+
+  dataSource: MatTableDataSource<Row> = new MatTableDataSource<Row>(this.currentData);
+
+  @ViewChild(MatTable) table!: MatTable<Row>;
+
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit() {
+    this.getData();
+    this.refreshTable();
+  }
+
+  getData(pageIndex: number = 0) {
+    // this.http.get(``).subscribe(res => {});
+  }
+
+  refreshTable() {
+    this.table.renderRows();
+  }
+
+  onPageChange(pageIndex: number) {
+    this.getData(pageIndex);
+    this.refreshTable();
+  }
+}
+
+interface Row {
+  guid: number;
+  type: number;
+  datetime: Date;
+  account: number;
+  name: string;
+  level: number;
+  race: number;
+  class: number;
+  gender: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort'; 
 
 import { AppComponent } from './app.component';
 
@@ -14,9 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
     MatToolbarModule,
-    ScrollingModule
+    MatTableModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
