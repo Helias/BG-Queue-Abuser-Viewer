ee34e43cbf1d2eb8e23496dd695ba137464be984
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2022-07-17 22:37:04 +0200

    Organized imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatMenuModule } from '@angular/material/menu';
-
 import { AppComponent } from './app.component';
 import { FilterComponent } from './filter/filter.component';
-import { FormsModule } from '@angular/forms';
 import { LegendComponent } from './legend/legend.component';
 
 @NgModule({
@@ -36,7 +33,6 @@ import { LegendComponent } from './legend/legend.component';
     MatTooltipModule,
     MatMenuModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
