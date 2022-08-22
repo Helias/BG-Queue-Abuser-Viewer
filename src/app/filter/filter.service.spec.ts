d5792e5834b6d26f4311c8cb4edaf7abe39f58c6
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2022-08-22 23:08:26 +0200

    Added tests

--- src/app/filter/filter.service.spec.ts
+++ src/app/filter/filter.service.spec.ts
@@ -13,4 +13,37 @@ describe('FilterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have values$ Observable', () => {
+    expect(service.values$).toBeDefined();
+  });
+
+  it('should have the right values', () => {
+    service.values$.subscribe(values => {
+      expect(values).toEqual({
+        currentPage: 1,
+        entriesPerPage: 20
+      });
+    });
+  });
+
+  it('should have setState method', () => {
+    expect(service.setState).toBeDefined();
+  });
+
+  it('should update state successfully', () => {
+    service.setState({
+      currentPage: 2,
+      entriesPerPage: 50,
+      nameFilter: 'test'
+    });
+
+    service.values$.subscribe(values => {
+      expect(values).toEqual({
+        currentPage: 2,
+        entriesPerPage: 50,
+        nameFilter: 'test'
+      });
+    });
+  });
 });
