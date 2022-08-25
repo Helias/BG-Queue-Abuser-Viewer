a275323605a95a02cb0cb8fcaa1448c8c2e2f43d
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2022-08-25 23:15:29 +0200

    Improved tests

--- src/app/filter/filter.service.spec.ts
+++ src/app/filter/filter.service.spec.ts
@@ -1,4 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { Filters } from './filter.model';
 
 import { FilterService } from './filter.service';
 
@@ -10,40 +12,43 @@ describe('FilterService', () => {
     service = TestBed.inject(FilterService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
-  it('should have values$ Observable', () => {
+  it('should have values$', () => {
     expect(service.values$).toBeDefined();
+    expect(service.values$).toBeInstanceOf(BehaviorSubject<Filters>);
   });
 
-  it('should have the right values', () => {
+  it('values$ have the right values', fakeAsync(() => {
     service.values$.subscribe(values => {
       expect(values).toEqual({
         currentPage: 1,
         entriesPerPage: 20
       });
     });
-  });
+  }));
 
-  it('should have setState method', () => {
+  it('should have setState', () => {
     expect(service.setState).toBeDefined();
+    expect(service.setState).toBeInstanceOf(Function);
   });
 
-  it('should update state successfully', () => {
-    service.setState({
+  it('should update state successfully', fakeAsync(() => {
+    const before: Filters = {
+      currentPage: 1,
+      entriesPerPage: 20
+    };
+
+    const after: Filters = {
       currentPage: 2,
       entriesPerPage: 50,
       nameFilter: 'test'
+    };
+
+    let state = before;
+    service.values$.subscribe(values => {
+      expect(values).toEqual(state);
     });
 
-    service.values$.subscribe(values => {
-      expect(values).toEqual({
-        currentPage: 2,
-        entriesPerPage: 50,
-        nameFilter: 'test'
-      });
-    });
-  });
+    state = after;
+    service.setState(after);
+  }));
 });
