f3ab345d1d209c65a757104ac49ed0fddbf3b0e4
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2023-06-20 23:00:55 +0200

    Added specs for testing the state change of the filter component

--- src/app/filter/filter.component.spec.ts
+++ src/app/filter/filter.component.spec.ts
@@ -1,23 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { FilterComponent } from './filter.component';
 
 describe('FilterComponent', () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
 
+  const initialState = {
+    currentPage: 1,
+    entriesPerPage: 20,
+    nameFilter: null
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FilterComponent ]
-    })
-    .compileComponents();
+      imports: [MatIconModule, ReactiveFormsModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FilterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the right initial state', () => {
+    expect(component.filterForm.value).toEqual(initialState);
+  });
+
+  it('should update filter state without name filter', () => {
+    const withoutNameFilter: Required<typeof component.filterForm.value> = {
+      currentPage: 2,
+      entriesPerPage: 50,
+      nameFilter: null
+    };
+
+    component.filterForm.setValue(withoutNameFilter);
+    component.updateFilters();
+
+    expect(component.filterForm.value).toEqual({
+      currentPage: withoutNameFilter.currentPage,
+      entriesPerPage: withoutNameFilter.entriesPerPage,
+      nameFilter: null
+    });
+  });
+
+  it('should update filter state with name filter', () => {
+    const withNameFilter: Required<typeof component.filterForm.value> = {
+      currentPage: 2,
+      entriesPerPage: 50,
+      nameFilter: 'test'
+    };
+
+    component.filterForm.setValue(withNameFilter);
+    component.updateFilters();
+
+    expect(component.filterForm.value).toEqual({
+      currentPage: withNameFilter.currentPage,
+      entriesPerPage: withNameFilter.entriesPerPage,
+      nameFilter: withNameFilter.nameFilter
+    });
   });
 });
